a0b7703f46ac871265dea3084c8cec3988e81d68
jaydamon <Heron2128#>
2021-03-27 09:15:34 -0400

    Add gradle build file

---
+++ spring-boot-gradle.groovy
@@ -0,0 +1,49 @@
+node {
+
+    def VERSION
+    def IMAGE_NAME_TAG
+    def REPOSITORY = 'repository.factotumsoftware.com'
+
+    
+    try {
+        stage ("Checkout") {
+       
+            checkout scm: [
+                $class: 'GitSCM', 
+                userRemoteConfigs: [
+                    [
+                        url: "https://github.com/JayDamon/${PROJECT_NAME}.git", 
+                        credentialsId: 'GitHub'
+                    ]
+                ],
+                branches: [
+                    [
+                        name: "${BRANCH_SELECTOR}"
+                    ]
+                ],
+            ], poll: false
+            
+            VERSION = ./gradlew properties --no-daemon --console=plain -q | grep "^version:" | awk '{printf $2}'
+            echo VERSON
+            IMAGE_NAME_TAG = "${REPOSITORY}/${PROJECT_NAME}:${VERSION}"
+            currentBuild.displayName = "${ENVIRONMENT}-${VERSION}"
+        }
+        
+        // stage ("Build Image") {
+            
+        //     sh "docker build -t ${IMAGE_NAME_TAG} ."
+
+        // }
+        
+        // stage ("Push Container to Registry") {
+
+        //     withCredentials([usernamePassword(credentialsId: 'DockerRegistry', passwordVariable: 'psw', usernameVariable: 'usr')]) {
+        //         sh 'docker login -u ${usr} -p ${psw} https://repository.factotumsoftware.com'
+        //         sh "docker push ${IMAGE_NAME_TAG}"
+        //     }
+        // }
+
+    } finally {
+        cleanWs()
+    }
+}
